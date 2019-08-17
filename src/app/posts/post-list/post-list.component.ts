50a96b001104544ae3d7438c4bef01acea97815f
alejo128 <ALEJO128@Alejandros-MacBook-Pro.local>
2019-08-16 21:59:25 -0500

    se hicieron modificaciones y se agregaron boton para editar y para eliminar todavia no funcionales

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
     selector: 'app-post-list',
@@ -9,11 +11,26 @@ import { Post } from '../post.model';
 
 })
 
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
     /*posts =  [
         {title: 'First post', content: 'This is the first post\s content'},
         {title: 'Second post', content: 'This is the second post\s content'},
         {title: 'Third post', content: 'This is the third post\s content'},
     ];*/
-    @Input() posts: Post[] = [];
+    posts: Post[] = [];
+    private postsSub: Subscription;
+
+    constructor(public postsService: PostsService) {}
+
+    ngOnInit() {
+        this.posts = this.postsService.getPosts();
+        this.postsService.getPostUpdateListener()
+            .subscribe((posts: Post[]) => {
+                this.posts = posts;
+            });
+    }
+
+    ngOnDestroy() {
+        this.postsSub.unsubscribe();
+    }
 }
